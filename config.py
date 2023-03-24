410305496b37e2868b0b1bf4484de1166f47dcd1
Filmotainment <89825644+filmotainment@users.noreply.github.com>
2023-03-24 16:36:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
 import os
 
 # Telegram Bot API token
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
 
 # OpenAI API key
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
-# Language model ID
-OPENAI_MODEL_ID = os.environ.get("OPENAI_MODEL_ID")
+# OpenAI language model ID
+OPENAI_MODEL_ID = os.environ.get('OPENAI_MODEL_ID')
+
+# MongoDB URI for user data storage (optional)
+MONGODB_URI = os.environ.get('MONGODB_URI')
+
+# Pyrogram API ID and API hash
+PYROGRAM_API_ID = os.environ.get('PYROGRAM_API_ID')
+PYROGRAM_API_HASH = os.environ.get('PYROGRAM_API_HASH')
+
+# Pyrogram bot session name
+PYROGRAM_SESSION_NAME = os.environ.get('PYROGRAM_SESSION_NAME')
