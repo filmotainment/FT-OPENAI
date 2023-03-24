ab7234c919e198b85c8650f5589051fe444a2501
Filmotainment <89825644+filmotainment@users.noreply.github.com>
2023-03-24 16:28:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Telegram Bot API token
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+# OpenAI API key
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# Language model ID
+OPENAI_MODEL_ID = os.environ.get("OPENAI_MODEL_ID")
